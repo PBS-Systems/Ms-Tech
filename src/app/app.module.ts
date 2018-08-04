3cae1225c66946a9b890ec30ba84f8bcabb59a3e
pallabidya <pallabidya@gmail.com>
2018-08-04 18:39:12 +0530

    Header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
+
 @NgModule({
   declarations: [
     AppComponent,
